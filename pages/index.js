965f322d85f6a47b6f04bb694013ce81bf8b06ce
LemonSea <2533566560@qq.com>
2020-08-10 23:19:28 +0800

    dev:babel 配置与 antd 按需导入

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Head from 'next/head';
+import {Button} from 'antd';
 
 const Home = () => (
   <>
     <Head>
       <title>Home</title>
+      <Button>ces</Button>
     </Head>
  </>
 )
